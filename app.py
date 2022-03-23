fe72d87609fd73b5f1cf00315dcf49f35eaabc49
KIRAishere-2000 <82574274+KIRAishere-2000@users.noreply.github.com>
2022-03-23 11:57:01 +0530

    Uploaded project files

---
+++ app.py
@@ -0,0 +1,51 @@
+import time
+import edgeiq
+
+
+def main():
+    pose_estimator = edgeiq.PoseEstimation("alwaysai/human_pose")
+    pose_estimator.load(engine=edgeiq.Engine.DNN)
+
+    print("Loaded model:\n{}\n".format(pose_estimator.model_id))
+    print("Engine: {}".format(pose_estimator.engine))
+    print("Accelerator: {}\n".format(pose_estimator.accelerator))
+
+    fps = edgeiq.FPS()
+
+    try:
+        with edgeiq.WebcamVideoStream(cam=0) as video_stream, \
+                edgeiq.Streamer() as streamer:
+            # Allow Webcam to warm up
+            time.sleep(2.0)
+            fps.start()
+
+            # loop detection
+            while True:
+                frame = video_stream.read()
+                results = pose_estimator.estimate(frame)
+                # Generate text to display on streamer
+                text = ["Model: {}".format(pose_estimator.model_id)]
+                text.append(
+                        "Inference time: {:1.3f} s".format(results.duration))
+                for ind, pose in enumerate(results.poses):
+                    text.append("Person {}".format(ind))
+                    text.append('-'*10)
+                    text.append("Key Points:")
+                    for key_point in pose.key_points:
+                        text.append(str(key_point))
+                streamer.send_data(results.draw_poses(frame), text)
+
+                fps.update()
+
+                if streamer.check_exit():
+                    break
+    finally:
+        fps.stop()
+        print("elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
+        print("approx. FPS: {:.2f}".format(fps.compute_fps()))
+
+        print("Program Ending")
+
+
+if __name__ == "__main__":
+    main()
